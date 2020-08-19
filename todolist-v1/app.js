134d5de5e290b4f4e4e82b04ddd421dd3dd33f81
HieuNguyenDinh <hieu.nd1995@gmail.com>
2020-08-19 19:53:47 -0400

    add template

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,11 +5,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items= ['hello'];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));  
+
 app.get("/", function(req, res){
-  var day = 'today';
-  res.render('list', {curDay: day});
+  
+  var today = new Date();
+
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  var day = today.toLocaleDateString('en-US', options);
+  res.render('list', {curDay: day, newItemList: items});
+});
+
+app.post("/", (req,res) => {
+  items.push(req.body.newItem);
+
+  res.redirect("/");
+
 });
 
 app.listen(3000, function(){
